2d74bba4be7635a6d20d203cb844bb97791af3b6
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-07 19:27:23 +0530

    bsearch imple

---
+++ dsax/search/BinarySearch.py
@@ -0,0 +1,18 @@
+def binarysearch(A: list[int], target: int) -> list[int]:
+	left = 0
+	right = len(A) - 1
+	
+	while left <= right:
+		mid = (left + right) // 2
+		if A[mid] == target:
+			return mid
+		elif A[mid] < target:
+			left = mid + 1
+		else:
+			right = mid - 1
+	return None
+	
+if __name__ == "__main__":	
+	A = [1, 2, 3, 4, 5]
+	result = binarysearch(A, target = 2)
+	print(result)
