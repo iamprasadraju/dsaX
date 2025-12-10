397d8c308be9e6c67a7c1efe1e1cb885d70489a7
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-10 18:05:00 +0530

    test for Bubble sort

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,13 +1,15 @@
 import unittest
 from dsax.sort.SelectionSort import selection_sort
+from dsax.sort.BubbleSort import bubble_sort
 from dsax.helpers import generate
 
 class TestSorting(unittest.TestCase):
 	def test_SelectionSort(self):
-		for _, arr in generate(upper_limit=1000):
+		for _, arr in generate():
 			self.assertEqual(selection_sort(arr, reverse=True), sorted(arr, reverse=True))
-
-
+	def test_BubbleSort(self):
+		for _, arr in generate():
+			self.assertEqual(bubble_sort(arr), sorted(arr))
 
 if __name__ == "__main__":
 	unittest.main()
