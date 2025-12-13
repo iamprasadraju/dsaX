bbedc61ceea34e433d650f1fe36e73a25c3bd30d
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-13 12:15:25 +0530

    added tests for insertion and bubble sorts

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,11 +1,20 @@
 import unittest
-from dsax.sort import selectionsort
+from dsax.sort import selectionsort, bubblesort, insertionsort
 from dsax.helpers import generate
 
 class TestSorting(unittest.TestCase):
 	def test_SelectionSort(self):
 		for _, arr in generate():
-			self.assertEqual(selectionsort(arr, reverse=True), sorted(arr, reverse=True))
+			self.assertEqual(selectionsort(arr), sorted(arr))
+			
+	def test_BubbleSort(self):
+		for _, arr in generate():
+			self.assertEqual(bubblesort(arr), sorted(arr))
+			
+	def test_InsertionSort(self):
+		for _, arr in generate():
+			self.assertEqual(insertionsort(arr), sorted(arr))
+			
 	
 
 if __name__ == "__main__":
